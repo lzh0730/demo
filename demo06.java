4e8dcf5cd62cde85a07fb67d048ec03f99a6bb8f
lzh0730 <96362619+lzh0730@users.noreply.github.com>
2023-07-03 10:21:49 +0800

    Add files via upload

---
+++ demo06.java
@@ -0,0 +1,13 @@
+public class demo06 {
+    public static void main(String[] args) {
+        //格式1  数据类型【】 数组名
+        //int 【】 array
+        //格式二  数据类型 数组名【】
+        //int array【】
+
+
+        //静态初始化   数据类型【】数组名=new 数据类型【】{元素1、元素2}
+        int a=11;
+        System.out.println(a);
+    }
+}
