fa84648714dd331d895fbf53a0f1842ada6f0363
hhh <605629414@qq.com>
2023-07-04 15:27:01 +0800

    修改

--- demo2.java
+++ demo2.java
@@ -1,6 +1,27 @@
+import java.util.jar.JarEntry;
+
 public class demo2 {
     public static void main(String[] args) {
-        int a=12;
-        System.out.println(a);
+        //数组
+        /*索引
+        1.获取数组里面的元素
+        2.格式：数组名【索引】
+         */
+        int[] arr = {1,2,3,4};
+        int number =arr[0];
+        System.out.println(number);
+
+
+        //可以直接打印
+        System.out.println(arr[1]);
+
+
+        //快速遍历
+        for (int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+
+
     }
 }
+
